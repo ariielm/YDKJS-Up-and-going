ea5a4ffb476aa4a1318167181b4aff7c40078471
bld_aalarcon <bld_aalarcon@uolinc.com>
2016-08-03 18:01:33 -0300

    Chapter 2 - Going...

--- Chapter2.js
+++ Chapter2.js
@@ -116,5 +116,37 @@ a == b; // false
 
 
 // ----- Variables ----- //
+//---
+var a = 2;
+foo();
 
+function foo() {
+    a = 3;
+    console.log( a ); // 3
+    var a;
+}
+console.log( a ); // 2
+//---
+
+//---
+function foo() {
+    var a = 1;
+    if (a >= 1) {
+        let b = 2;
+        while (b < 5) {
+            let c = b * 2;
+            b++;
+            console.log( a + c );
+        }
+        //the c var (let) is not accessible here
+    }
+    //the b var (let) is not accessible here
+}
+foo(); // 5 7 9
+//---
 // ----- Variables ----- //
+
+
+// ----- Conditionals ----- //
+
+// ----- Conditionals ----- //
